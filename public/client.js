5cad8dad7fa9d928eca7455ead8eecef95c30eb1
John Son <40287191+son7211@users.noreply.github.com>
2024-03-24 00:43:58 +0000

    계산기 기능 추가 및 버그 수정

--- public/client.js
+++ public/client.js
@@ -33,6 +33,12 @@ function calculate(operand1, operand2, operation) {
         case '/':
             uri += "?operation=divide";
             break;
+        case '%':
+            uri += "?operation=modulus";
+            break;
+        case '^':
+            uri += "?operation=power";
+            break;
         default:
             setError();
             return;
